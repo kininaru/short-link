e40ac9521038ab4b4b0993dd6109cb69aa8b51bf
Kininaru <shiftregister233@outlook.com>
2021-10-26 21:39:51 +0800

    add missed semicolon

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ addEventListener("fetch", (event) => {
 async function handleRequest(request) {
   let path = "/" + request.url.split("/")[3];
   let forwardLink = await ShortLink.get(path);
-  console.log(`${path},${forwardLink},${Date.now()}`)
+  console.log(`${path},${forwardLink},${Date.now()}`);
   return Response.redirect(forwardLink, 302);
 }
