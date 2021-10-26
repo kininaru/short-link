fa124fa6479b6404cc70a769f88be60663fe5d4a
Kininaru <shiftregister233@outlook.com>
2021-10-26 21:07:55 +0800

    302

---
+++ index.js
@@ -0,0 +1,13 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+async function handleRequest(request) {
+  let path = "/" + request.url.split("/")[3];
+  let forwardLink = await ShortLink.get(path);
+  return new Response().redirect(forwardLink, 302);
+}
