504d8a6a7f90a141434724785e03b7d024bdfc9a
Kininaru <shiftregister233@outlook.com>
2021-10-27 21:33:15 +0800

    format code

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 addEventListener("fetch", (event) => {
-  event.respondWith(
-    handleRequest(event.request).catch(
-      (err) => new Response(err.stack, { status: 500 })
-    )
-  );
+    event.respondWith(handleRequest(event.request).catch(err => new Response(err.stack, {status: 500})));
 });
 
 async function handleRequest(request) {
-  let path = "/" + request.url.split("/")[3];
-  let forwardLink = await ShortLink.get(path);
-  if (forwardLink === null) return new Response("Link not found.", {
-    status: 404,
-  });
-  console.log(`${path},${forwardLink},${Date.now()}`);
-  return Response.redirect(forwardLink, 302);
+    let path = "/" + request.url.split("/")[3];
+    let forwardLink = await ShortLink.get(path);
+    if (forwardLink === null) return new Response("Link not found.", {status: 404});
+    console.log(`${path},${forwardLink},${Date.now()}`);
+    return Response.redirect(forwardLink, 302);
 }
